a5898b57345948211c1b29ab6d1f5897a896cfb4
Fatih Genç <10bachiko@gmail.com>
2021-12-31 22:24:20 +0300

    outsource types for reducer

---
+++ client/src/components/reducer-types/user-reducer-types.ts
@@ -0,0 +1,24 @@
+import { User } from '../../models/user';
+
+export enum UserActionTypes {
+    USER_SIGN_IN = 'USER_SIGN_IN',
+    USER_SIGN_UP = 'USER_SIGN_UP',
+    USER_LOG_OUT = 'USER_LOG_OUT',
+}
+
+export interface UserActionPayload {
+    token?: string;
+    errMessage?: string;
+}
+
+export interface UserAction {
+    type: UserActionTypes;
+    payload?: UserActionPayload;
+}
+
+export interface UserState {
+    isAuth: boolean;
+    currentUser: User | null;
+    token: string | null;
+    authError: string | null;
+}
