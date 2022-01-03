1c6e5368cd3aa1cb9d90f330ef6af166db18ebc7
Fatih Genç <10bachiko@gmail.com>
2022-01-03 17:59:17 +0300

    change event type in change handler

--- client/src/components/hooks/use-input.tsx
+++ client/src/components/hooks/use-input.tsx
@@ -1,46 +1,52 @@
 import React, { useState } from 'react';
 
 const useInput = (
-  validationMessage: string,
-  initValue: string | boolean,
-  validate: (input: string) => boolean
+    validationMessage: string,
+    initValue: string | boolean,
+    validate: (input: string) => boolean
 ) => {
-  const [enteredInput, setEnteredInput] = useState<boolean | string>(initValue);
-  const [isTouched, setIsTouched] = useState<boolean>(false);
+    const [enteredInput, setEnteredInput] = useState<boolean | string>(
+        initValue
+    );
+    const [isTouched, setIsTouched] = useState<boolean>(false);
 
-  const valueIsValid = validate(enteredInput as string) || false;
+    const valueIsValid = validate(enteredInput as string) || false;
 
-  const resetValue = (val: string | boolean) => {
-    setEnteredInput(val);
-    setIsTouched(false);
-  };
+    const resetValue = (val: string | boolean) => {
+        setEnteredInput(val);
+        setIsTouched(false);
+    };
 
-  const blurHandler = () => {
-    setIsTouched(true);
-  };
+    const blurHandler = () => {
+        setIsTouched(true);
+    };
 
-  const hasError = !valueIsValid && isTouched;
+    const hasError = !valueIsValid && isTouched;
 
-  const changeHandler = (e: React.ChangeEvent<HTMLInputElement>) =>
-    setEnteredInput((prevState) => {
-      if (typeof prevState === 'boolean') {
-        return !prevState;
-      }
+    const changeHandler = (
+        e:
+            | React.ChangeEvent<HTMLInputElement>
+            | React.ChangeEvent<HTMLTextAreaElement>
+    ) =>
+        setEnteredInput((prevState) => {
+            if (typeof prevState === 'boolean') {
+                return !prevState;
+            }
 
-      setIsTouched(true);
+            setIsTouched(true);
 
-      return e.target.value;
-    });
+            return e.target.value;
+        });
 
-  return {
-    inputValue: enteredInput,
-    inputIsValid: valueIsValid,
-    inputHasError: hasError,
-    valMessage: validationMessage,
-    onChange: changeHandler,
-    onBlur: blurHandler,
-    resetInput: resetValue,
-  };
+    return {
+        inputValue: enteredInput,
+        inputIsValid: valueIsValid,
+        inputHasError: hasError,
+        valMessage: validationMessage,
+        onChange: changeHandler,
+        onBlur: blurHandler,
+        resetInput: resetValue,
+    };
 };
 
 export default useInput;
