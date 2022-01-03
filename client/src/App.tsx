f13cb805b69cb828d7e9cc84ca90a11c4fd19ff5
Fatih Genç <10bachiko@gmail.com>
2022-01-03 18:02:42 +0300

    remove unnecessary imports

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,16 +2,13 @@ import React from 'react';
 import Router from './components/Router';
 import UserContextProvider from './components/store/UserProvider';
 import './index.css';
-// import { BlogCrudServiceClient } from './proto/BlogServiceServiceClientPb';
-// import { InsertBlogRequest } from './proto/blogService_pb';
-// import * as timestamp_pb from 'google-protobuf/google/protobuf/timestamp_pb';
 
 function App() {
-  return (
-    <UserContextProvider>
-      <Router />
-    </UserContextProvider>
-  );
+    return (
+        <UserContextProvider>
+            <Router />
+        </UserContextProvider>
+    );
 }
 
 export default App;
