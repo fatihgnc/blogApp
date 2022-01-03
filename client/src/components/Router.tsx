5e0616d86fe56cff1bc3974fe01c13337467a074
Fatih Genç <10bachiko@gmail.com>
2022-01-03 17:58:31 +0300

    add /addBlog to router

--- client/src/components/Router.tsx
+++ client/src/components/Router.tsx
@@ -1,29 +1,31 @@
 import { Route, Routes } from 'react-router-dom';
 import MainLayout from '../MainLayout';
 import BlogsScreen from './screens/BlogsScreen';
-import FriendsScreen from './screens/ProfileScreen';
+import ProfileScreen from './screens/ProfileScreen';
 import HomeScreen from './screens/HomeScreen';
 import LoginAndRegisterScreen from './screens/LoginAndRegisterScreen';
 import BlogContextProvider from './store/BlogProvider';
+import AddBlogScreen from './screens/AddBlogScreen';
 
 const Router = () => {
-  return (
-    <Routes>
-      <Route path='/' element={<MainLayout />}>
-        <Route
-          index
-          element={
-            <BlogContextProvider>
-              <HomeScreen />
-            </BlogContextProvider>
-          }
-        />
-        <Route path='profile' element={<FriendsScreen />} />
-        <Route path='blogs' element={<BlogsScreen />} />
-        <Route path='auth' element={<LoginAndRegisterScreen />} />
-      </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path='/' element={<MainLayout />}>
+                <Route
+                    index
+                    element={
+                        <BlogContextProvider>
+                            <HomeScreen />
+                        </BlogContextProvider>
+                    }
+                />
+                <Route path='profile' element={<ProfileScreen />} />
+                <Route path='blogs' element={<BlogsScreen />} />
+                <Route path='addBlog' element={<AddBlogScreen />} />
+                <Route path='auth' element={<LoginAndRegisterScreen />} />
+            </Route>
+        </Routes>
+    );
 };
 
 export default Router;
