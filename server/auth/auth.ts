e716b9a04b41b0a277a08b63be9c3ca810b7c49f
Fatih Genç <10bachiko@gmail.com>
2022-01-03 18:11:06 +0300

    remove authtoken field from user model, and some error handling in auth.ts

--- server/auth/auth.ts
+++ server/auth/auth.ts
@@ -2,117 +2,135 @@ import grpc from '@grpc/grpc-js';
 import { UserModel } from '../db/mongoose';
 import { UserInfo__Output } from '../proto/blogApp/UserInfo';
 import { SignInAndUpResponse } from '../proto/blogApp/SignInAndUpResponse';
-import { jwtUser, signJWT, verifyJWT } from './jwt';
+import jwt from 'jsonwebtoken';
+
+type jwtUser = {
+    _id: string;
+    username: string;
+    password: string;
+    blogCount: number;
+    iat?: number;
+    exp?: number;
+};
 
 export async function signUserUp(
-  call: grpc.ServerUnaryCall<UserInfo__Output, SignInAndUpResponse>
+    call: grpc.ServerUnaryCall<UserInfo__Output, SignInAndUpResponse>
 ): Promise<{ err: string | null; token: string | null }> {
-  // get the username,request and check if they are set
-  const { username, password } = call.request;
+    // get the username,request and check if they are set
+    const { username, password } = call.request;
 
-  if (!username || !password)
-    return { err: 'both username and password are required!', token: null };
+    if (!username || !password)
+        return { err: 'both username and password are required!', token: null };
 
-  // check to see if user with this username exists in db
-  const userInDb = await UserModel.findOne({ username }).exec();
+    // check to see if user with this username exists in db
+    const userInDb = await UserModel.findOne({ username }).exec();
 
-  if (userInDb) return { err: 'username is in use!', token: null };
+    if (userInDb) return { err: 'username is in use!', token: null };
 
-  // create the new user, create the jwt
-  const user = new UserModel({ username, password });
+    // create the new user, create the jwt
+    const user = new UserModel({ username, password });
 
-  const { blogCount, _id } = user;
+    const { blogCount, _id } = user;
 
-  const authToken = signJWT({
-    _id,
-    username,
-    password,
-    blogCount,
-  });
+    const authToken = jwt.sign(
+        {
+            _id,
+            username,
+            password,
+            blogCount,
+        },
+        'mykey',
+        { expiresIn: '1m' }
+    );
 
-  user.authToken = authToken;
+    try {
+        // save user to db
+        await user.save();
 
-  try {
-    // save user to db
-    await user.save();
-
-    return { err: null, token: authToken };
-  } catch (err) {
-    return { err, token: null };
-  }
+        return { err: null, token: authToken };
+    } catch (err) {
+        return { err, token: null };
+    }
 }
 
 export async function signUserIn(
-  call: grpc.ServerUnaryCall<UserInfo__Output, SignInAndUpResponse>
+    call: grpc.ServerUnaryCall<UserInfo__Output, SignInAndUpResponse>
 ): Promise<{ err: string | null; token: string | null }> {
-  // get the data and validate it (check if null)
-  const { username, password } = call.request;
+    // get the data and validate it (check if null)
+    const { username, password } = call.request;
 
-  if (!username || !password)
-    return { err: 'both username and password are required!', token: null };
+    if (!username || !password)
+        return { err: 'both username and password are required!', token: null };
 
-  try {
-    // query the user with the given username and check if it exists
-    const user = await UserModel.findOne({ username }).exec();
+    try {
+        // query the user with the given username and check if it exists
+        const user = await UserModel.findOne({ username }).exec();
 
-    if (!user) return { err: 'no such user with this username!', token: null };
+        if (!user)
+            return { err: 'no such user with this username!', token: null };
 
-    // user exists and we check if passwords match
-    if (user.password !== password)
-      return { err: 'credentials are incorrect', token: null };
+        // user exists and we check if passwords match
+        if (user.password !== password)
+            return { err: 'credentials are incorrect', token: null };
 
-    // credentials are valid now get the info from the user and sign the jwt and return it
-    const { blogCount, _id } = user;
+        // credentials are valid now get the info from the user and sign the jwt and return it
+        const { blogCount, _id } = user;
 
-    const authToken = signJWT({
-      _id,
-      username,
-      password,
-      blogCount,
-    });
+        const authToken = jwt.sign(
+            {
+                _id,
+                username,
+                password,
+                blogCount,
+            },
+            'mykey',
+            { expiresIn: '10s' }
+        );
 
-    user.authToken = authToken;
-    await user.save();
+        await user.save();
 
-    return { err: null, token: authToken };
-  } catch (err) {
-    return { err, token: null };
-  }
+        return { err: null, token: authToken };
+    } catch (err) {
+        return { err, token: null };
+    }
 }
 
 export async function logUserOut(authToken: string) {
-  // check token
-  if (!authToken) return 'authToken is required!';
-  // verify token
-  const payload = verifyJWT(authToken) as jwtUser;
-  // verify payload
-  if (!payload) return 'invalid token';
-  // get username from token
-  const { username } = payload;
-  // query user
-  try {
-    const user = await UserModel.findOne({ username }).exec();
-    // check user
-    if (!user) {
-      return 'no user exists with this username';
-    }
-    // delete it's authtoken field
-    user.authToken = undefined;
-    // save it
-    await user.save();
+    // check token
+    if (!authToken) return 'authToken is required!';
+    // verify token
+    const payload = jwt.verify(authToken, 'mykey') as jwtUser;
+    console.log(payload);
+    // verify payload
+    if (!payload) return 'invalid token';
+    // get username from token
+    const { username } = payload;
+    // query user
+    try {
+        const user = await UserModel.findOne({ username }).exec();
+        // check user
+        if (!user) {
+            return 'no user exists with this username';
+        }
+        // save it
+        await user.save();
 
-    return 'deleted auth token successfully';
-  } catch (err) {
-    throw err;
-  }
+        return 'deleted auth token successfully';
+    } catch (err) {
+        throw err;
+    }
 }
 
 export async function getUserFromToken(authToken: string) {
-  // get the user payload from token and extract the id
-  const userPayload = verifyJWT(authToken) as jwtUser;
-  const { _id } = userPayload;
+    // get the user payload from token and extract the id
+    try {
+        const userPayload = jwt.verify(authToken, 'mykey') as jwtUser;
+        const { _id } = userPayload;
 
-  // query the user with id
-  const user = await UserModel.findById(_id).exec();
-  return user;
+        // query the user with id
+        const user = await UserModel.findById(_id).exec();
+        return user;
+    } catch (error) {
+        throw error;
+    }
 }
--- server/db/mongoose.ts
+++ server/db/mongoose.ts
@@ -1,71 +1,67 @@
 import { Types, Schema, model, connect } from 'mongoose';
 
 interface UserObj {
-  username: string;
-  password: string;
-  blogCount: number;
-  authToken: string;
+    username: string;
+    password: string;
+    blogCount: number;
 }
 
 interface BlogObj {
-  blogTitle: string;
-  blogContent: string;
-  authorId: Types.ObjectId;
+    blogTitle: string;
+    blogContent: string;
+    authorId: Types.ObjectId;
 }
 
 const userSchema = new Schema<UserObj>({
-  username: {
-    type: String,
-    trim: true,
-    required: true,
-    unique: true,
-    min: [4, 'Username should be at least 4 characters!'],
-    max: [27, 'Username should at most 27 characters!'],
-  },
-  password: {
-    type: String,
-    trim: true,
-    required: true,
-    min: [6, 'Username should be at least 6 characters!'],
-    max: [31, 'Username should at most 31 characters!'],
-  },
-  blogCount: {
-    type: Number,
-    default: 0,
-  },
-  authToken: {
-    type: String,
-  },
+    username: {
+        type: String,
+        trim: true,
+        required: true,
+        unique: true,
+        min: [4, 'Username should be at least 4 characters!'],
+        max: [27, 'Username should at most 27 characters!'],
+    },
+    password: {
+        type: String,
+        trim: true,
+        required: true,
+        min: [6, 'Username should be at least 6 characters!'],
+        max: [31, 'Username should at most 31 characters!'],
+    },
+    blogCount: {
+        type: Number,
+        default: 0,
+    },
 });
 
 const blogSchema = new Schema<BlogObj>({
-  blogTitle: {
-    type: String,
-    unique: true,
-    trim: true,
-    required: true,
-    min: [10, 'Username should be at least 10 characters!'],
-    max: [50, 'Username should at most 50 characters!'],
-  },
-  blogContent: {
-    type: String,
-    trim: true,
-    required: true,
-    min: [50, 'Username should be at least 50 characters!'],
-    max: [500, 'Username should at most 500 characters!'],
-  },
-  authorId: {
-    type: Types.ObjectId,
-    required: true,
-    ref: 'User',
-  },
+    blogTitle: {
+        type: String,
+        unique: true,
+        trim: true,
+        required: true,
+        min: [10, 'Username should be at least 10 characters!'],
+        max: [50, 'Username should at most 50 characters!'],
+    },
+    blogContent: {
+        type: String,
+        trim: true,
+        required: true,
+        min: [50, 'Username should be at least 50 characters!'],
+        max: [500, 'Username should at most 500 characters!'],
+    },
+    authorId: {
+        type: Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
 });
 
 const UserModel = model<UserObj>('User', userSchema);
 const BlogModel = model<BlogObj>('Blog', blogSchema);
 
 async function run(): Promise<void> {
-  await connect('mongodb://localhost:27017/blogApp');
+    await connect('mongodb://localhost:27017/blogApp');
 }
 
 export { UserModel, BlogModel, run as connectMongo };
